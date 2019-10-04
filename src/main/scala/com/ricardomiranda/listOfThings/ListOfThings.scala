c11062cadfc21338c82c36decc0311902b8b1eb6
Ricardo Miranda <mail@ricardoMiranda.com>
2019-10-04 09:49:23 +0100

    deleted object menu

--- src/main/scala/com/ricardomiranda/listOfThings/ListOfThings.scala
+++ src/main/scala/com/ricardomiranda/listOfThings/ListOfThings.scala
@@ -3,26 +3,20 @@ package com.ricardomiranda.listOfThings
 case class Item(name: String, price: Double)
 
 
-case class Menu(items: List[Item])
-
-object Menu {
-  def apply(): Unit = {
-    val cola = Item(name = "Cola", price = 0.50)
-    val coffe = Item(name = "Coffe", price = 1.00)
-    val cheeseSandwich = Item(name = "Cheese Sandwich", price = 2.00)
-    val steakSandwich = Item(name = "SteakSandwich", price = 4.5)
-    val menu = List(cola, coffe, cheeseSandwich, steakSandwich)
-
-    new Menu(items = menu)
-  }
+case class Menu(items: List[Item] = List(
+  Item(name = "Cola", price = 0.50),
+  Item(name = "Coffe", price = 1.00),
+  Item(name = "SteakSandwich", price = 4.5),
+  Item(name = "SteakSandwich", price = 4.5))) {
 
   def price(item: String, menu: Menu): Double = {
     menu.items.filter(_ == item).head.price
   }
 }
 
-object shopping {
+object Shopping {
+  val menu = new Menu()
   def total(items: List[String], menu: Menu): Double = {
-    items.map(i => Menu.price(i, menu)).sum
+    items.map(i => menu.price(i, menu)).sum
   }
 }
