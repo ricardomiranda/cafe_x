26cc385b0d678ef5dcfea2c05c0781e4d9e02a54
Ricardo Miranda <mail@ricardoMiranda.com>
2019-10-14 23:49:37 +0100

    Service charge computed and tested

--- src/main/scala/com/ricardomiranda/cafeX/CafeX.scala
+++ src/main/scala/com/ricardomiranda/cafeX/CafeX.scala
@@ -1,22 +1,42 @@
 package com.ricardomiranda.cafeX
 
+import scala.math.max
 
-case class Menu(items: Map[String, Double] = Map(
-  "Cola" -> 0.50,
-  "Coffee" -> 1.00,
-  "Cheese Sandwich" -> 2.0,
-  "Steak Sandwich" -> 4.5)) {
+sealed trait FoodType
+case object Drink extends FoodType
+case object ColdFood extends FoodType
+case object HotFood extends FoodType
 
-  def price(item: String): Double = this.items(item)
+case class Item(price: Double, foodType: FoodType)
+
+case class Menu(items: Map[String, Item] = Map(
+  "Cola" -> Item(price = 0.50, foodType = Drink),
+  "Coffee" -> Item(price = 1.00, foodType = Drink),
+  "Cheese Sandwich" -> Item(price = 2.0, foodType = ColdFood),
+  "Steak Sandwich" -> Item( price =  4.5, foodType = HotFood))) {
+
+  def price(item: String): Double = this.items(item).price
+
+  def ItemsByType(foodType: FoodType): List[String] = this.items.filter(_._2.foodType == foodType).map(_._1).toList
 }
 
 object Shopping {
-  def total(items: List[String], menu: Menu): Double = {
-    items.map(menu.price(_)).sum
-  }
+  def total(items: List[String], menu: Menu): Double = items.map(menu.price(_)).sum
 }
 
-object serviceCharge {
-  val hotFood: Set[String] = Set("Steak Sandwich")
-  val food: Set[String] = Set("Cheese Sandwich", "Steak Sandwich")
+object ServiceCharge {
+  def containsTypeOfFood(items: List[String], menu: Menu, foodType: FoodType): Boolean = items.toSet.intersect(menu.ItemsByType(foodType = foodType).toSet) match {
+    case  xs if xs.size == 0 => false
+    case _ => true
+  }
+
+  def containsDrinks(items: List[String], menu: Menu): Boolean = this.containsTypeOfFood(items = items, menu = menu, foodType = Drink)
+  def containsColdFood(items: List[String], menu: Menu): Boolean = this.containsTypeOfFood(items = items, menu = menu, foodType = ColdFood)
+  def containsHotfood(items: List[String], menu: Menu): Boolean = this.containsTypeOfFood(items = items, menu = menu, foodType = HotFood)
+
+  def serviceValue(items: List[String], menu: Menu, bill: Double): Double = items match {
+    case items if this.containsHotfood(items = items, menu = menu) => max(20.00, 0.20 * bill)
+    case items if this.containsColdFood(items = items, menu = menu) => 0.10 * bill
+    case _ => 0.00
+  }
 }
--- src/test/scala/com/ricardomiranda/cafeX/CafeXSpec.scala
+++ src/test/scala/com/ricardomiranda/cafeX/CafeXSpec.scala
@@ -13,6 +13,25 @@ class CafeXSpec extends FunSuite {
     assert(actual == expected)
   }
 
+
+  test("List of drinks") {
+    val expected: List[String] = List("Cola", "Coffee")
+    val actual: List[String] = menu.ItemsByType(foodType = Drink)
+    assert(actual == expected)
+  }
+
+  test("List of hot food") {
+    val expected: List[String] = List("Cheese Sandwich")
+    val actual: List[String] = menu.ItemsByType(foodType = ColdFood)
+    assert(actual == expected)
+  }
+
+  test("List of cold food") {
+    val expected: List[String] = List("Steak Sandwich")
+    val actual: List[String] = menu.ItemsByType(foodType = HotFood)
+    assert(actual == expected)
+  }
+
   test("Total Price of empty shopping list") {
     val items: List[String] = List()
 
@@ -50,6 +69,47 @@ class CafeXSpec extends FunSuite {
 
     val expected: Double = 3.50
     val actual: Double = Shopping.total(items = items, menu = menu)
+    assert(actual == expected)
+  }
+
+  test("Empty list does not contains drinks") {
+    val items: List[String] = List()
+
+    val actual: Boolean = ServiceCharge.containsDrinks(items = items, menu = menu)
+    assert(!actual)
+  }
+
+  test("List with Cola contains drinks") {
+    val items: List[String] = List("Cola")
+
+    val actual: Boolean = ServiceCharge.containsDrinks(items = items, menu = menu)
+    assert(actual)
+  }
+
+  test("Service charge for empty list is 0.00") {
+    val items: List[String] = List()
+    val bill: Double = Shopping.total(items = items, menu = menu)
+
+    val expected: Double = 0.00
+    val actual: Double = ServiceCharge.serviceValue(items = items, menu = menu, bill = bill)
+    assert(actual == expected)
+  }
+
+  test("Service charge for list of drinks is 0.00") {
+    val items: List[String] = List("Cola", "Cola", "Coffee")
+    val bill: Double = Shopping.total(items = items, menu = menu)
+
+    val expected: Double = 0.00
+    val actual: Double = ServiceCharge.serviceValue(items = items, menu = menu, bill = bill)
+    assert(actual == expected)
+  }
+
+  test("Service charge for list of with hot food is 20.00 max") {
+    val items: List[String] = List("Cola", "Cola", "Coffee", "Steak Sandwich", "Steak Sandwich", "Steak Sandwich", "Steak Sandwich")
+    val bill: Double = Shopping.total(items = items, menu = menu)
+
+    val expected: Double = 20.00
+    val actual: Double = ServiceCharge.serviceValue(items = items, menu = menu, bill = bill)
     assert(actual == expected)
   }
 }
