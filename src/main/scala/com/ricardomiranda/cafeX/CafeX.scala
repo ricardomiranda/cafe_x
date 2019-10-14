1cdbc386bdea144eddd1231aa7d0addacc6ea6ea
Ricardo Miranda <mail@ricardoMiranda.com>
2019-10-14 12:56:56 +0100

    tests and menu map

--- src/main/scala/com/ricardomiranda/cafeX/CafeX.scala
+++ src/main/scala/com/ricardomiranda/cafeX/CafeX.scala
@@ -1,23 +1,17 @@
 package com.ricardomiranda.cafeX
 
-case class Item(name: String, price: Double)
 
+case class Menu(items: Map[String, Double] = Map(
+  "Cola" -> 0.50,
+  "Coffee" -> 1.00,
+  "SteakSandwich" -> 4.5,
+  "SteakSandwich" -> 4.5)) {
 
-case class Menu(items: List[Item] = List(
-  Item(name = "Cola", price = 0.50),
-  Item(name = "Coffe", price = 1.00),
-  Item(name = "SteakSandwich", price = 4.5),
-  Item(name = "SteakSandwich", price = 4.5))) {
-
-  def price(item: String, menu: Menu): Double = {
-    menu.items.filter(_ == item).head.price
-  }
+  def price(item: String): Double = this.items(item)
 }
 
 object Shopping {
-  val menu = new Menu()
-
   def total(items: List[String], menu: Menu): Double = {
-    items.map(i => menu.price(i, menu)).sum
+    items.map(menu.price(_)).sum
   }
 }
--- src/test/scala/com/ricardomiranda/cafeX/CafeXSpec.scala
+++ src/test/scala/com/ricardomiranda/cafeX/CafeXSpec.scala
@@ -3,10 +3,21 @@ package com.ricardomiranda.cafeX
 import org.scalatest.FunSuite
 
 class CafeXSpec extends FunSuite {
-  val menu = new Menu()
+  val menu: Menu = new Menu()
 
-  test("Prie of 1 coffe") {
-    val items = List("Coffe")
-    assert(Shopping.total(items = items, menu = menu) == 0)
+  test("Price of coffee") {
+    val item: String = "Coffee"
+
+    val expected: Double = 1.00
+    val actual: Double = menu.price(item)
+    assert(actual == expected)
+  }
+
+  test("Total Price of empty shopping list") {
+    val items: List[String] = List()
+
+    val expected: Double = 1
+    val actual: Double = Shopping.total(items = items, menu = menu)
+    assert(actual == expected)
   }
 }
