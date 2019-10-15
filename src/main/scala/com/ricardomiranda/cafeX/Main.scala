544184974ef42b10112c301372cf4c5a4da61915
Ricardo Miranda <mail@ricardoMiranda.com>
2019-10-15 10:39:06 +0100

    Interaction with user

--- src/main/scala/com/ricardomiranda/cafeX/Main.scala
+++ src/main/scala/com/ricardomiranda/cafeX/Main.scala
@@ -1,16 +1,32 @@
 package com.ricardomiranda.cafeX
 
+import scala.annotation.tailrec
+
 object Main extends App {
   val menu: Menu = new Menu()
 
-  val items: List[String] = List("Coffee")
+  val items: List[String] = itemsToPurchase(List())
   val bill: Double = Shopping.total(items= items, menu = menu)
   val serviceCharge: Double = ServiceCharge.serviceValue(items = items, menu = menu, bill = bill)
-  val totalCost: Double = bill + serviceCharge
+  this.bill(items = items, bill = bill, serviceCharge = serviceCharge)
 
-  println("With items:")
-  items.foreach(println(_))
-  println(s"Price of items is ${bill}")
-  println(s"Service charge is ${serviceCharge}")
-  println(s"Total cost is ${totalCost}")
+  @tailrec
+  def itemsToPurchase(items: List[String]): List[String] = {
+    readLine(
+      """Insert name of item (press Enter to finish your purchase)?
+        |""".stripMargin).trim match {
+      case "" => items
+      case i => itemsToPurchase(items = i :: items)
+    }
+  }
+
+  def bill(items: List[String], bill: Double, serviceCharge: Double): Unit = {
+    val totalCost: Double = bill + serviceCharge
+
+    println("With items:")
+    items.foreach(println(_))
+    println(s"Price of items is ${bill}")
+    println(s"Service charge is ${serviceCharge}")
+    println(s"Total cost is ${totalCost}")
+  }
 }
